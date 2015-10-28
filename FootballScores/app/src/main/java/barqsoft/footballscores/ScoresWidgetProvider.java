1852f9fd4efdf4d82a8e90ff1d6e7e9e3644221d
Hardy Arora <harora@akamai.com>
2015-10-28 19:43:11 -0400

    - Widget added. - Building. - Widget shows up in list

--- FootballScores/app/src/main/java/barqsoft/footballscores/ScoresWidgetProvider.java
+++ FootballScores/app/src/main/java/barqsoft/footballscores/ScoresWidgetProvider.java
@@ -1,7 +1,32 @@
 package barqsoft.footballscores;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
 /**
  * Created by harora on 10/28/15.
  */
-public class ScoresWidgetProvider {
+public class ScoresWidgetProvider extends AppWidgetProvider{
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        String scores = "123 - 213";
+
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(),
+                    R.layout.football_scores_widget);
+
+
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.football_scores_widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
 }
